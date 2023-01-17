14d14087cd125047c8db8660a884cbb1b57ee8ed
qsda82 <39626315+qsda82@users.noreply.github.com>
2023-01-17 21:51:52 +0800

    確認第一筆交易

--- verify.py
+++ verify.py
@@ -1,6 +1,3 @@
-"""
-處理驗證程序
-"""
 import RSA_key_operation
 import json
 import datetime
@@ -15,7 +12,7 @@ def make_pk(j):
     #print(public_key)
     #key = RSA.import_key(public_key)
     return public_key
-
+#驗證是否過期
 def validity_verify(j):
     timenow = datetime.datetime.today()
     after=datetime.datetime.strptime(j["certificate"]["Validity"]["Not After"],'%b %d %H:%M:%S %Y GMT')
@@ -23,31 +20,41 @@ def validity_verify(j):
         return 1
     else:
         return 0
-
+#驗證是否撤銷
 def revocation_verify(j):
     revocation=str(j["certificate"]["Revocation"])
-    if revocation=="Flase" :
+    if revocation=="False" :
         return 1
     else:
         return 0
-
+#驗證使用次數
 def useage_verify(j,useage_times):
     Transparnecy_list=iota_operation.msg_index_info(j["certificate"]["Transparnecy"])
     if len(Transparnecy_list) >= useage_times:
         return 1
     else:
         return 0
-
+#取得新的證書
 def cert_up_to_date(index):
     cid_list=iota_operation.msg_index_info(index)
-    return cid_list[0]
-
+    time_list=[]
+    for i in cid_list:
+        cert_json=iota_operation.msg_raw(i)#取得證書內容
+        j=json.loads(cert_json)
+        time=datetime.datetime.strptime(j["certificate"]["Validity"]["Not Before"],'%b %d %H:%M:%S %Y GMT')
+        time_list.append(time)
+    for i in range(len(time_list)):
+        if time_list[i] == max(time_list):
+            return cid_list[i]
+    
+#證書驗證
 def cert_verify(cid,useage_times):
     index=iota_operation.msg_get_index(cid)#根據cid取得其index
     newest_cid=cert_up_to_date(index)#根據index取得最新的cid值
     if newest_cid == cid:#檢查證書鏈是否為最新的證書
         cert_json=iota_operation.msg_raw(cid)#取得證書內容
         j=json.loads(cert_json)#將證書存為json檔
+        print(j)
         pubkey=make_pk(j)#製作成公鑰檔
         message = json.dumps(j["certificate"]).encode('utf-8')#取得證書簽章
         sign=RSA_key_operation.sign_verify(message,bytes.fromhex(j["Signature"]["Sign"]),pubkey)#簽章驗證
